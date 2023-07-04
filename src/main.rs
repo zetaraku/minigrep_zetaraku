ee221e4f0c9d2ea53ac8212fc369fc284f9c65ac
Raku Zeta <zetaraku@gmail.com>
2023-07-04 14:14:57 +0800

    ✨ read content from file

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for `{query}` in file `{file_path}`");
+
+
+    let contents = std::fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
