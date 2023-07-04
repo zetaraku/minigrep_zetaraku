03192c9d9a6083b6f5fd3b5ab2eeaa200758a2aa
Raku Zeta <zetaraku@gmail.com>
2023-07-04 16:27:44 +0800

    ✅ implement search()

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,15 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
