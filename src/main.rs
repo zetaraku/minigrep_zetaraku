93a8257cd7825cb84b982ef3519d2f7d42607139
Raku Zeta <zetaraku@gmail.com>
2023-07-04 14:13:02 +0800

    ✨ assign args to variables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    dbg!(args);
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for `{query}` in file `{file_path}`");
 }
