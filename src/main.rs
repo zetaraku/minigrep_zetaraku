455683be147072db9075d4184d275dcccc4670e2
Raku Zeta <zetaraku@gmail.com>
2023-07-04 18:22:32 +0800

    🩹 output error message to stderr instead

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         std::process::exit(1);
     });
 
     minigrep::run(config).unwrap_or_else(|err|{
-        println!("Application error: {err}");
+        eprintln!("Application error: {err}");
         std::process::exit(1);
     });
 }
